3bd8037f2a9bdbe3d3d73985fd254ba30b03d6d0
Adam Smith <smith.adam937@gmail.com>
2022-06-07 13:27:43 -0500

    Wind, Humidity, and Weather Icon Added for Forecast

--- script.js
+++ script.js
@@ -4,16 +4,31 @@ var citiesBtnEl = document.querySelector('#city-buttons');
 var forecastEl = document.querySelector('#forecast');
 var todayDateEl = document.querySelector('#today-date');
 var todayTempEl = document.querySelector('#today-temp');
-var dateOneTempEl = document.querySelector('#temp1');
-var dateTwoTempEl = document.querySelector('#temp2');
-var dateThreeTempEl = document.querySelector('#temp3');
-var dateFourTempEl = document.querySelector('#temp4');
-
 
 var dateOneEl = document.querySelector('#date1');
 var dateTwoEl = document.querySelector('#date2');
 var dateThreeEl = document.querySelector('#date3');
 var dateFourEl = document.querySelector('#date4');
+
+var iconOneEl = document.querySelector('#icon1');
+var iconTwoEl = document.querySelector('#icon2');
+var iconThreeEl = document.querySelector('#icon3');
+var iconFourEl = document.querySelector('#icon4');
+
+var tempOneEl = document.querySelector('#temp1');
+var tempTwoEl = document.querySelector('#temp2');
+var tempThreeEl = document.querySelector('#temp3');
+var tempFourEl = document.querySelector('#temp4');
+
+var HumidityOneEl = document.querySelector('#humidity1');
+var HumidityTwoEl = document.querySelector('#humidity2');
+var HumidityThreeEl = document.querySelector('#humidity3');
+var HumidityFourEl = document.querySelector('#humidity4');
+
+var windOneEl = document.querySelector('#wind1');
+var windTwoEl = document.querySelector('#wind2');
+var windThreeEl = document.querySelector('#wind3');
+var windFourEl = document.querySelector('#wind4');
 
 var globalDatesArray = []; 
 var maxTemp = []; 
@@ -72,6 +87,7 @@ var getCityWeather = function(lat, lon){
     response.json().then(function(data){
       convertDate(data);
       convertTemp(data);
+      getWeatherDetails(data);
       //console.log(data.daily[0].dt);
     })
 
@@ -99,7 +115,7 @@ var getCityWeather = function(lat, lon){
     dateThreeEl.textContent = globalDatesArray[3];
     dateFourEl.textContent = globalDatesArray[4];
     console.log(globalDatesArray);
-  }
+}
 
   var convertTemp = function (data){
     console.log(data.daily[0].temp.max);
@@ -109,9 +125,37 @@ var getCityWeather = function(lat, lon){
     }
     maxTemp = maximumTemp;
     todayTempEl.textContent = "Temp: " + maxTemp[0] + " °F";
-    dateOneTempEl.textContent = "Temp: " + maxTemp[1] + " °F";
-    dateTwoTempEl.textContent = "Temp: " + maxTemp[2] + " °F";
-    dateThreeTempEl.textContent = "Temp: " + maxTemp[3] + " °F";
-    dateFourTempEl.textContent = "Temp: " + maxTemp[4] + " °F";
+    tempOneEl.textContent = "Temp: " + maxTemp[1] + " °F";
+    tempTwoEl.textContent = "Temp: " + maxTemp[2] + " °F";
+    tempThreeEl.textContent = "Temp: " + maxTemp[3] + " °F";
+    tempFourEl.textContent = "Temp: " + maxTemp[4] + " °F";
+}
+
+var getWeatherDetails = function(data) {
+  var humidity = []; 
+  var wind = []; 
+  var icon = []; 
+  for (var i = 1; i <= 5; i++){
+    humidity.push(data.daily[i].humidity);
+    wind.push(data.daily[i].wind_speed);
+    icon.push(data.daily[i].weather[0].icon);
   }
+  HumidityOneEl.textContent = "Humidity: " + humidity[1] + " %";
+  HumidityTwoEl.textContent = "Humidity: " + humidity[2] + " %"; 
+  HumidityThreeEl.textContent = "Humidity: " + humidity[3] + " %";
+  HumidityFourEl.textContent = "Humidity: " + humidity[4] + " %";
+
+  windOneEl.textContent = "Wind: " + wind[1] + " MPH";
+  windTwoEl.textContent = "Wind: " + wind[2] + " MPH";
+  windThreeEl.textContent = "Wind: " + wind[3] + " MPH";
+  windFourEl.textContent = "Wind: " + wind[4] + " MPH";
+
+  
+  console.log(icon);
+
+  iconOneEl.setAttribute('src','http://openweathermap.org/img/wn/' + icon[1] + ".png");
+  iconTwoEl.setAttribute('src', 'http://openweathermap.org/img/wn/' + icon[2] + ".png");
+  iconThreeEl.setAttribute('src', 'http://openweathermap.org/img/wn/' + icon[3] + ".png"); 
+  iconFourEl.setAttribute('src', 'http://openweathermap.org/img/wn/' + icon[4] + ".png"); 
+}
  
